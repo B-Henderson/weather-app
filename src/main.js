6beb427f8801ffbaa0701921269ad6c8c8d5d514
B-Henderson <ben.henderson@soprasteria.com>
2017-05-04 15:16:35 +0100

    added a little error tolerence

--- src/main.js
+++ src/main.js
@@ -2,81 +2,86 @@ var angular = require('angular');
 require('./scss/main.scss');
 var app = angular.module('app', []);
 
-app.config(['$httpProvider', function($httpProvider) {
-  $httpProvider.defaults.useXDomain = true;
-  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+app.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }]);
 
 
-app.filter('days', function($filter) {
-  return function(input, name) {
-    if (input) {
-      var formatting = $filter('date')(input, 'dd-MMMM');
-      var today = $filter('date')(new Date(), 'dd-MMMM');
-      if (formatting === today && name) {
-        return 'Today ';
-      }
-      var tomorrow = new Date();
-      tomorrow = tomorrow.setDate(tomorrow.getDate() + 1);
-      tomorrow = $filter('date')(tomorrow, 'dd-MMMM');
-      if (formatting === tomorrow && name) {
-        return 'Tomorrow';
-      }
+app.filter('days', function ($filter) {
+    return function (input, name) {
+        if (input) {
+            var formatting = $filter('date')(input, 'dd-MMMM');
+            var today = $filter('date')(new Date(), 'dd-MMMM');
+            if (formatting === today && name) {
+                return 'Today ';
+            }
+            var tomorrow = new Date();
+            tomorrow = tomorrow.setDate(tomorrow.getDate() + 1);
+            tomorrow = $filter('date')(tomorrow, 'dd-MMMM');
+            if (formatting === tomorrow && name) {
+                return 'Tomorrow';
+            }
 
-      if (name) {
-        return $filter('date')(input, 'EEEE');
-      }
-      return formatting
-    }
-  };
+            if (name) {
+                return $filter('date')(input, 'EEEE');
+            }
+            return formatting
+        }
+    };
 })
 
-app.factory('serviceFactory', function($http, provideWeatherFactory) {
-  var serviceFactory = function(param) {
-    return $http({
-      method: 'GET',
-      url: 'https://crossorigin.me/https://www.metaweather.com/api/location/' + param + '/'
-    });
-  }
-  return serviceFactory;
+app.factory('serviceFactory', function ($http, provideWeatherFactory) {
+    var serviceFactory = function (param) {
+        return $http({
+            method: 'GET',
+            url: 'https://crossorigin.me/https://www.metaweather.com/api/location/' + param + '/'
+        });
+    }
+    return serviceFactory;
 });
-app.factory('provideWeatherFactory', function($http) {
-  var getweatherJson = function(param) {
-    return $http({
-      method: 'GET',
-      url: 'https://crossorigin.me/https://www.metaweather.com/api/location/search/?query=' + param
-    })
-  }
-  return getweatherJson;
+app.factory('provideWeatherFactory', function ($http) {
+    var getweatherJson = function (param) {
+        return $http({
+            method: 'GET',
+            url: 'https://crossorigin.me/https://www.metaweather.com/api/location/search/?query=' + param
+        })
+    }
+    return getweatherJson;
 });
 
 
 function wController($scope, provideWeatherFactory, serviceFactory, daysFilter, $filter) {
-  this.noLocation = null;
-  $scope.loading = false;
-  this.getReport = function(param) {
-    $scope.loading = true;
-    provideWeatherFactory(param).then(function(result) {
-      serviceFactory(result.data[0].woeid).then(function(result) {
-        $scope.weather = result.data;
-        $scope.loading = false;
-        displayResults();
-      });
-    });
-  }
+    this.noLocation = null;
+    $scope.loading = false;
+    this.getReport = function (param) {
+        $scope.loading = true;
+        provideWeatherFactory(param).then(function (result) {
+            if (result.data.length > 0) {
+                serviceFactory(result.data[0].woeid).then(function (result) {
+                    $scope.weather = result.data;
+                    $scope.loading = false;
+                    displayResults();
+                });
+            } else {
+                $scope.loading = false;
+                console.log('there was an error');
+            }
+        });
+    }
 
-  var displayResults = function() {
-    $scope.displaying = true;
-    $scope.fiveDay = $scope.weather.consolidated_weather;
-  }
+    var displayResults = function () {
+        $scope.displaying = true;
+        $scope.fiveDay = $scope.weather.consolidated_weather;
+    }
 };
 
 
 angular.module('app').component('widgetComponent', {
-  template: require('./templates/main.template.html'),
-  controller: wController,
-  controllerAs: 'widget'
+    template: require('./templates/main.template.html'),
+    controller: wController,
+    controllerAs: 'widget'
 });
 app.component('loading', {
-  template: require('./templates/loader.template.html')
+    template: require('./templates/loader.template.html')
 });
